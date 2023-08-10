a01e2269a6d80649de8b8e09c606608b2121cd16
Jose Gutierrez <gutierrez.jose24@gmail.com>
2023-08-10 05:42:26 -0500

    phase 2 lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Links from "./Links"; // Import the Links component
-import user from "../data/user"; // Import the user data
+import Links from "./Links";
 
-function About({ bio }) {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {bio && bio !== "" && <p>{bio}</p>}
+      {bio && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links github={user.links.github} linkedin={user.links.linkedin} />
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,16 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// Import the user data
+// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home name={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
+      <About github={user.links.github} linkedin={user.links.linkedin} bio={user.bio} />
     </div>
   );
 }
